f5b8496da99e5d7976984879140790ec5dcf43b4
mobosinfo <mobosinfo@gmail.com>
2018-04-11 16:48:01 +0300

    auth storyboard was added

---
+++ Concert Assistant/Concert Assistant/AuthScViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AuthScViewController.swift
+//  Concert Assistant
+//
+//  Created by MOBOS on 03.04.2018.
+//  Copyright © 2018 Frites. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class AuthViewController: UIViewController {
+    
+}
